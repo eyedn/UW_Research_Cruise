0d53ad09cfae1f36b33ebec77a36cfbd8648a4ab
Aydin Karatas <alckaratas18@gmail.com>
2023-09-20 21:50:54 -0700

    added line to make sure script is run in its dir

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@
 ###############################################################################
 #!/bin/bash
 
+
+cd "$(dirname "$0")" # this makes sure to run script in script's directory
 clear
 echo "Running calculations..."
 python3 main.py data.txt
